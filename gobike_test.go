69105d07c91ac59b76cec0da1fb9d020a388206f
Kyle Conroy <kyle@kyleconroy.com>
2018-08-09 00:10:47 -0700

    Run tests over the official datasets
    I've added a `golden.csv` file which contains the first 50 records from
    the January 2018 Ford GoBike trip data set.
    
    If any of the CSVs from the official dataset are placed into the
    testdata directory, the test suite will automatically discover and run
    the tests and benchmarks using those files in addition to the golden
    set.

--- gobike_test.go
+++ gobike_test.go
@@ -2,17 +2,20 @@ package gobike
 
 import (
 	"bufio"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
 func TestLoad(t *testing.T) {
-	f, err := os.Open("data/201806-fordgobike-tripdata.csv")
+	f, err := os.Open(filepath.Join("testdata", "golden.csv"))
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer f.Close()
-	trips, err := Load(bufio.NewReader(f))
+	trips, err := Load(f)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -24,7 +27,7 @@ func TestLoad(t *testing.T) {
 func BenchmarkLoad(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		f, err := os.Open("data/201806-fordgobike-tripdata.csv")
+		f, err := os.Open(filepath.Join("testdata", "golden.csv"))
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -43,5 +46,80 @@ func BenchmarkLoad(b *testing.B) {
 		if err := f.Close(); err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+func officialDatasets(tb testing.TB) []string {
+	tb.Helper()
+	files, err := ioutil.ReadDir("testdata")
+	if err != nil {
+		tb.Fatal(err)
+	}
+	csvs := []string{}
+	for _, file := range files {
+		if !strings.HasSuffix(file.Name(), "-fordgobike-tripdata.csv") {
+			continue
+		}
+		csvs = append(csvs, filepath.Join("testdata", file.Name()))
+	}
+	return csvs
+}
+
+func TestLoadOfficial(t *testing.T) {
+	paths := officialDatasets(t)
+	if len(paths) == 0 {
+		t.Skip("No FordGo CSVs in the testdata directory")
+	}
+	for _, tripdata := range paths {
+		trippath := tripdata
+		t.Run(filepath.Base(trippath), func(t *testing.T) {
+			f, err := os.Open(trippath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
+			trips, err := Load(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(trips) == 0 {
+				t.Fatal("expected to see non-zero trips, got 0")
+			}
+		})
+	}
+}
+
+func BenchmarkLoadOfficial(b *testing.B) {
+	paths := officialDatasets(b)
+	if len(paths) == 0 {
+		b.Skip("No FordGo CSVs in the testdata directory")
+	}
+
+	for _, tripdata := range paths {
+		trippath := tripdata
+		b.Run(filepath.Base(trippath), func(b *testing.B) {
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				f, err := os.Open(trippath)
+				if err != nil {
+					b.Fatal(err)
+				}
+				inf, err := f.Stat()
+				if err != nil {
+					b.Fatal(err)
+				}
+				b.SetBytes(inf.Size())
+				trips, err := Load(bufio.NewReader(f))
+				if err != nil {
+					b.Fatal(err)
+				}
+				if len(trips) == 0 {
+					b.Fatal("expected to see non-zero trips, got 0")
+				}
+				if err := f.Close(); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
 	}
 }

049dc65770434ae967e0cbc27c3e32976f0f0efb
Kyle Conroy <kyle@kyleconroy.com>
2018-08-13 15:01:59 -0700

    Add `handlers.Log` to the server output

--- cmd/gobike-server/main.go
+++ cmd/gobike-server/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/kevinburke/handlers"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("docs"))
-	http.Handle("/", fs)
+	http.Handle("/", handlers.Log(fs))
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8333", nil))
 }

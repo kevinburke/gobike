35799f89d8f2ae8e0b394d5d7b671f2decd29cd4
Kyle Conroy <kyle@kyleconroy.com>
2018-08-13 00:08:02 -0700

    Make the trips-per-day cmd general
    Also add the new dataset command. It prepares all the data into one CSV
    that can be imported into BigQuery / Postgres

---
+++ README.md
@@ -0,0 +1,49 @@
+# Datasets
+
+In addition to server, we've made the Ford GoBike datasets available online.
+
+## BigQuery
+
+All trip data lives in the
+[ford_gobike](https://bigquery.cloud.google.com/table/kjc-datasets:ford_gobike.trips)
+dataset, which is availably publicly.
+
+### Trips per week
+
+```sql
+SELECT
+  DATE_TRUNC(DATE(start_time), WEEK) as week,
+  COUNT(*) as trips
+FROM `kjc-datasets.ford_gobike.trips`
+GROUP BY 1
+ORDER BY 1
+```
+
+### Unique bikes per week
+
+```sql
+SELECT
+  DATE_TRUNC(DATE(start_time), WEEK) as week,
+  COUNT(distinct bike_id) as bikes
+FROM `kjc-datasets.ford_gobike.trips`
+GROUP BY 1
+ORDER BY 1
+```
+
+### Average trips per bike per week
+
+```sql
+WITH bike_trips AS (
+SELECT
+  DATE_TRUNC(DATE(start_time), WEEK) as week,
+  bike_id,
+  count(*) as trips
+FROM `kjc-datasets.ford_gobike.trips`
+GROUP BY 1, 2
+ORDER BY 1
+)
+
+SELECT week, avg(trips)
+FROM bike_trips
+GROUP BY 1
+```
---
+++ cmd/gobike-dataset/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/kevinburke/gobike"
+	"github.com/kevinburke/gobike/geo"
+)
+
+func run() error {
+	var format = flag.String("format", "csv", "CSV format")
+	flag.Parse()
+
+	trips, err := gobike.LoadDir(flag.Arg(0))
+	if err != nil {
+		return err
+	}
+	sort.Slice(trips, func(i, j int) bool {
+		return trips[i].StartTime.Before(trips[j].StartTime)
+	})
+
+	fw, err := os.Create(flag.Arg(1))
+	if err != nil {
+		return err
+	}
+	defer fw.Close()
+
+	w := csv.NewWriter(fw)
+
+	geolocate := func(lat, long float64) string {
+		if geo.SF.ContainsPoint(lat, long) {
+			return "San Francisco"
+		}
+		if geo.Oakland.ContainsPoint(lat, long) {
+			return "Oakland"
+		}
+		if geo.SanJose.ContainsPoint(lat, long) {
+			return "San Jose"
+		}
+		return ""
+	}
+
+	switch *format {
+	case "postgres":
+	default:
+		w.Write([]string{
+			"duration",
+			"start_time",
+			"end_time",
+			"start_station_id",
+			"start_station_name",
+			"start_station_latitude",
+			"start_station_longitude",
+			"start_station_city",
+			"end_station_id",
+			"end_station_name",
+			"end_station_latitude",
+			"end_station_longitude",
+			"end_station_city",
+			"bike_id",
+			"user_type",
+			"member_birth_year",
+			"member_gender",
+			"bike_share_for_all",
+		})
+	}
+
+	for i, trip := range trips {
+		if trip.EndTime.Before(trip.StartTime) {
+			// WTF?
+			continue
+		}
+
+		bikeshare := "false"
+		if trip.BikeShareForAllTrip {
+			bikeshare = "true"
+		}
+
+		var record []string
+
+		// TODO: Handle null values correctly
+		switch *format {
+		case "postgres":
+		default:
+			record = []string{
+				strconv.Itoa(int(trip.Duration.Seconds())),
+				trip.StartTime.Format(time.RFC3339),
+				trip.EndTime.Format(time.RFC3339),
+
+				// Start station
+				strconv.Itoa(trip.StartStationID),
+				trip.StartStationName,
+				fmt.Sprint(trip.StartStationLatitude),
+				fmt.Sprint(trip.StartStationLongitude),
+				geolocate(trip.StartStationLatitude, trip.StartStationLongitude),
+
+				// End Station
+				strconv.Itoa(trip.EndStationID),
+				trip.EndStationName,
+				fmt.Sprint(trip.EndStationLatitude),
+				fmt.Sprint(trip.EndStationLongitude),
+				geolocate(trip.EndStationLatitude, trip.EndStationLongitude),
+
+				strconv.Itoa(int(trip.BikeID)),
+				trip.UserType,
+				strconv.Itoa(trip.MemberBirthYear),
+				trip.MemberGender,
+				bikeshare,
+			}
+		}
+
+		if err := w.Write(record); err != nil {
+			return fmt.Errorf("error writing record %d: %s", i, err)
+		}
+	}
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
